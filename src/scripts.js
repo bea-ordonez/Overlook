9ab0a59efd47adbf0b5c500410f4f46cee88ed99
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-03 10:57:34 -0700

    Change styling, remove check out

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import fetchData from './apiCalls.js'
 import Booking from './Booking.js'
 import Customer from './Customer.js'
 import Room from './Room.js'
@@ -16,9 +16,13 @@ const searchBtn = document.querySelector('.search-btn');
 const bookingsBtn = document.querySelector('.bookings-btn')
 const dashboardView = document.querySelector('.dashboard-view')
 
+//Promise
+
+
 
 // GLOBAL Variables
 const booking = new Booking(bookingData[0])
+const room = new Room(roomData[0])
 
 // Event Listeners
 showPastBookings()
@@ -33,10 +37,10 @@ function showPastBookings() {
   dashboardView.innerHTML += `
     <div class="past-booking-info">
       <p>Booking Info</p>
-      <p>${booking.roomNumber}</p>
-      <p>Room Type</p>
-      <p>Room Cost</p>
-      <p>${booking.date}</p>
+      <p>Room Number: ${booking.roomNumber}</p>
+      <p>Room Type: ${room.roomType}</p>
+      <p>Total Cost: ${room.costPerNight}</p>
+      <p>Date: ${booking.date}</p>
    </div>`
 }
 
