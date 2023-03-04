7e66d0d4e888038aecc0d57bb77aa7413908d8f5
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-03 20:57:28 -0700

    Change id naming convention for classes

---
+++ test/Customer-test.js
@@ -0,0 +1,23 @@
+import {expect} from "chai";
+import Customer from "../src/Customer";
+import customerData from "../data/customerData";
+
+describe('Customer', () => {
+  let customer;
+
+  beforeEach( () => {
+    customer = new Customer(customerData[0]);
+  })
+
+  it('should be an instance of customer', () => {
+    expect(customer).to.be.an.instanceOf(Customer)
+  })
+
+  it('should have an id', () => {
+    expect(customer.id).to.equal(1)
+  })
+
+  it('should have a name', () => {
+    expect(customer.name).to.equal('Leatha Ullrich')
+  })
+})
