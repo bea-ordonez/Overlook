0fa28a187b2f695d89c4d597d8fb375770f2c8f5
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-05 20:50:10 -0700

    Add booking repository class and move corresponding methods

---
+++ src/BookingRepository.js
@@ -0,0 +1,15 @@
+class BookingRepository {
+    constructor(allBookingsArray) {
+      this.allBookings = allBookingsArray;
+    }
+
+    getCustomerBookingsById(id) {
+      return this.allBookings.filter(booking => booking.userID === id);
+    }
+
+    isRoomAvailable(roomNum, date) {
+        const matchedBooking = this.allBookings.find(booking => booking.roomNumber === roomNum && booking.date === date);
+        return !matchedBooking;
+    }
+}
+export default BookingRepository;
