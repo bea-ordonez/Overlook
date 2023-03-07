887a7b96a1d51a6a69883cb419dd31d8de2c31dd
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-03-07 08:56:21 -0700

    Fix tab spacing

--- src/RoomRepository.js
+++ src/RoomRepository.js
@@ -1,22 +1,22 @@
 import BookingRepository from "./BookingRepository";
 
 class RoomRepository {
-    constructor(allRoomsArray, bookingRepo) {
-        this.allRooms = allRoomsArray;
-        this.bookingRepo = bookingRepo;
-    }
+  constructor(allRoomsArray, bookingRepo) {
+    this.allRooms = allRoomsArray;
+    this.bookingRepo = bookingRepo;
+  }
 
-    calculateTotalSpent(bookings) {
-        let totalSpent = 0;
-        bookings.forEach(booking => {
-          const bookedRoom = this.allRooms.find(room => room.number === booking.roomNumber)
-          totalSpent += bookedRoom.costPerNight;
-        });
-        return totalSpent;
-    }
+  calculateTotalSpent(bookings) {
+    let totalSpent = 0;
+    bookings.forEach(booking => {
+    const bookedRoom = this.allRooms.find(room => room.number === booking.roomNumber)
+    totalSpent += bookedRoom.costPerNight;
+    });
+    return totalSpent;
+  }
 
-    getAvailableRooms(date, roomType) {
-        return this.allRooms.filter(room => this.bookingRepo.isRoomAvailable(room.number, date) && (roomType === room.roomType || roomType === "all room types"));
-    }
+  getAvailableRooms(date, roomType) {
+    return this.allRooms.filter(room => this.bookingRepo.isRoomAvailable(room.number, date) && (roomType === room.roomType || roomType === "all room types"));
+  }
 }
 export default RoomRepository;
